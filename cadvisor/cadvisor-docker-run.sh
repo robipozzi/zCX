9dc4cb30fbe488fbacef389967e9962690f850fa
Roberto Pozzi <r.robipozzi@gmail.com>
2021-01-27 21:06:46 +0100

    update

--- cadvisor/cadvisor-docker-run.sh
+++ cadvisor/cadvisor-docker-run.sh
@@ -1,5 +1,5 @@
 docker rm -f cadvisor
-CMD_RUN="docker run --name cadvisor --network monitoring_net -d -v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro ibmcom/cadvisor-s390x:0.33.0"
+CMD_RUN="docker run --name cadvisor --network monitoring_net -d -v /:/rootfs:ro -v /var/run:/var/run:ro -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro -v /dev/disk/:/dev/disk:ro ibmcom/cadvisor-s390x:0.36.0"
 echo Running Cadvisor with: $CMD_RUN
 $CMD_RUN
 
