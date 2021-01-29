ba6e1d6d24e93d2fcb1e020c7a5fffa7357bf4a1
Roberto Pozzi <r.robipozzi@gmail.com>
2021-01-29 10:17:26 +0100

    fix run scripts

--- nodeexporter/nodeexporter-docker-run.sh
+++ nodeexporter/nodeexporter-docker-run.sh
@@ -1,6 +1,4 @@
 docker rm -f nodeexporter
-CMD_RUN="docker run --name nodeexporter -d --network monitoring_net -p 9101:9100 -v /proc:/host/proc:ro -v /sys:/host/sys:ro -v /:/rootfs:ro -v /etc/hostname:/etc/host_hostname:ro nodeexporter:1.0"
-## zCX new version
-#CMD_RUN="docker run --name nodeexporter -d --network monitoring_net -p 9101:9100 -v /proc:/host/proc:ro -v /sys:/host/sys:ro -v /media:/rootfs/media:ro -v /etc/hostname:/etc/host_hostname:ro nodeexporter:1.0"
+CMD_RUN="docker run --name nodeexporter --network monitoring_net -d -p 9101:9100 -v /proc:/host/proc:ro -v /sys:/host/sys:ro -v /media:/rootfs/media:ro -v /etc/hostname:/etc/host_hostname:ro nodeexporter:1.0"
 echo Running Nodeexporter with: $CMD_RUN
 $CMD_RUN
